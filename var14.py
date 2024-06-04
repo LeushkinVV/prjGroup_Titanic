cdf862367448b4e128f84044c1a92c04f4d448ef
BooKonstantin <168420996+BooKonstantin@users.noreply.github.com>
2024-06-04 15:59:37 +0700

    Update var14.py

--- var14.py
+++ var14.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
+
 def var14_main():
     lines = get_data(filename)
     sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f = work(lines)
     show_web(sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f)
 
+
 filename = "data.csv"
+
 
 def show_web(sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f):
 
-#    st.image('Titanic.jpg')
-    st.header('Данные пассажиров Титаника')
+    data = ""
+    st.image('Titanic.jpg')
     st.write('Для просмотра числа родственников у выживших и погибших пассажиров, выберите пол пассажира')
     option = st.selectbox('Значение поля Sex:', ['Мужской', 'Женский'])
     if option == 'Мужской':
@@ -29,11 +32,13 @@ def show_web(sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f):
     plt.title("Колличество родственников у выживших и погибших пассажиров")
     st.pyplot(fig)
 
+
 def get_data(fn):
     with open (fn) as file:
         lines = file.readlines()
 
     return lines[1:]
+
 
 def work(lines):
     sum_liave_m = 0
@@ -66,8 +71,8 @@ def work(lines):
         sex = tmpR[1]
 
         # print("who: {} SibSp: {} Prach: {} sex: {}".format(who,SibSp, Parch, sex))
-        #if who.isdigit():
-    #Суммируем родственников выживших и погибших
+        # if who.isdigit():
+        # Суммируем родственников выживших и погибших
         if who == 0:
             if sex == 'male':
                 sum_dead_m = sum_dead_m + SibSp + Parch
@@ -78,16 +83,17 @@ def work(lines):
                 sum_liave_m = sum_liave_m + SibSp + Parch
             elif sex == 'female':
                 sum_liave_f = sum_liave_f + SibSp + Parch
-
     return (sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f)
 
 # lines = get_data(filename)
 # # print(lines)
-
+#
 # sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f = work(lines)
-
+#
 # # print("Родственников у погибших: m {} - f {}".format(sum_dead_m,sum_dead_f))
 # # print("Родственников у выживших: m {} - f {}".format(sum_liave_m, sum_liave_f))
+#
+# show_web(sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f)
 
-# # show_web(sum_liave_m, sum_liave_f, sum_dead_m, sum_dead_f)
-# var14_main()
+
+
