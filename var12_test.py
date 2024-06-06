f2839163add0970d3296a40263def53cd1f88c37
kittenjul <kunitsynauv@gmail.com>
2024-06-06 23:13:24 +0300

    Update var12_test.py

--- var12_test.py
+++ var12_test.py
@@ -6,7 +6,7 @@ def test_get_empty_data():
              '1,2,3,5,0,'',0,0',
              '1,2,0,5,8,'','',0',
              '0,0,0,0,0,0,0,0', ]
-    assert get_empty_data(lines,5) == (2, 50)
+    assert get_empty_data(lines, 5) == (2, 50)
 
 
 def test_get_empty_data_2():
@@ -14,7 +14,7 @@ def test_get_empty_data_2():
              '1,2,3,5,0,'',0,0',
              '1,2,0,5,8,'','',0',
              '0,0,0,'',0,0,0,0', ]
-    assert get_empty_data(lines,4) == (0, 0)
+    assert get_empty_data(lines, 4) == (0, 0)
 
 
 def test_get_empty_data_3_false():
@@ -22,5 +22,5 @@ def test_get_empty_data_3_false():
              '1,2,3.2,5,0,'',0,0',
              '1,2,0,5,8,'','',0',
              '0,0,0,0,0,0,0,0', ]
-    assert get_empty_data(lines,2) == (0, 0)
+    assert get_empty_data(lines, 2) == (0, 0)
     
