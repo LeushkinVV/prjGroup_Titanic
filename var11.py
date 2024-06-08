277589c4ee5866c88210e9f0011e9555f3d34c32
Arhlen <elarkhipova@yandex.ru>
2024-06-08 11:24:19 +0300

    Update var11.py

--- var11.py
+++ var11.py
@@ -17,7 +17,6 @@ def get_pas_count(lines, min_age):
     
 def var11_main():
     columns = ['до 30 лет', 'от 30 до 60 лет', 'старше 60 лет']
-    st.title('Пассажиры Титаника')
     st.write('Для просмотра информации о доле выживших/погибших пассажиров выберите возраст:')
     choice = st.selectbox('Выберите возраст:', columns)
     index = columns.index(choice)
